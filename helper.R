904ad66e939c17df5832a420a14ba2a30d59c19f
Lulliter <lmm76@georgetown.edu>
2024-04-30 15:49:36 +0200

    r/02_r_vscode.qmd

--- helper.R
+++ helper.R
@@ -1,9 +1,8 @@
-# Configuration in R x HTTPS ACCREDITATION
-
-
+# Configuration in R x HTTPS ACCREDITATION ----------------------------------------------------------
 # Required pckgs ----------------------------------------------------------
 if (!require("pacman")) {
 	install.packages('pacman' )
 }
 
 pacman::p_load(usethis, gitcreds, gh)
+
